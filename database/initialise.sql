966fa1abee94c01facaed68fbfff35e6a2e70aed
TheMCraft <business@themcraft.com>
2025-01-01 19:17:58 +0100

    fixed database syntax

--- database/initialise.sql
+++ database/initialise.sql
@@ -1,8 +1,8 @@
-CREATE TABLE IF NOT EXISTS "sociallink_users" (
-   "id" SERIAL PRIMARY KEY,
-   "discord_id" VARCHAR(255) UNIQUE NOT NULL,
-   "minecraft_token" VARCHAR(255) DEFAULT NULL,
-   "minecraft_uuid" VARCHAR(255) DEFAULT NULL,
-   "email" VARCHAR(255) DEFAULT NULL,
-   "created_at" TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+CREATE TABLE IF NOT EXISTS sociallink_users (
+    id SERIAL PRIMARY KEY,
+    discord_id VARCHAR(255) UNIQUE NOT NULL,
+    minecraft_token VARCHAR(255),
+    minecraft_uuid VARCHAR(255),
+    email VARCHAR(255),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );

997942e470b4f6a74f7675cf433e954b2fd93c38
TheMCraft <business@themcraft.com>
2025-01-01 18:25:11 +0100

    created test version for minecraft backend

---
+++ minecraft/backend/SocialLinkMCV/src/main/java/com/themcraft/socialLinkMCV/JoinListener.java
@@ -0,0 +1,21 @@
+package com.themcraft.socialLinkMCV;
+
+import net.md_5.bungee.api.ProxyServer;
+import net.md_5.bungee.api.event.PostLoginEvent;
+import net.md_5.bungee.api.plugin.Listener;
+import net.md_5.bungee.event.EventHandler;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Scanner;
+
+public class JoinListener implements Listener {
+    @EventHandler
+    public void onPostLogin(PostLoginEvent event) throws Exception {
+        String hostname = event.getPlayer().getPendingConnection().getVirtualHost().getHostName();
+        String uuid = event.getPlayer().getUniqueId().toString();
+        String token = hostname.split("\\.")[0];
+        String feedback = new Scanner(new URL("https://minergames.net/sociallink/api/minecraft/verify.php?token=" + token + "&uuid=" + uuid).openStream()).next();
+        ProxyServer.getInstance().getLogger().info("Player " + event.getPlayer().getName() + " joined thru " + hostname + " | " + feedback);
+    }
+}
